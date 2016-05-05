4630ce5e330b1c975a25cbe66fdab36a82724ddf
Evgeny Sugakov <shiroginne@gmail.com>
2016-05-05 16:39:16 +0300

    Add inline css spinner instead of gif

--- src/begateway.js
+++ src/begateway.js
@@ -1,6 +1,16 @@
 (function () {
   "use strict";
 
+  function cssStyleForSpinner() {
+    return "#circularG{position:relative;width:58px;height:58px;margin:auto}.circularG{position:absolute;background-color:#000;width:14px;height:14px;border-radius:9px;-o-border-radius:9px;-ms-border-radius:9px;-webkit-border-radius:9px;-moz-border-radius:9px;animation-name:bounce_circularG;-o-animation-name:bounce_circularG;-ms-animation-name:bounce_circularG;-webkit-animation-name:bounce_circularG;-moz-animation-name:bounce_circularG;animation-duration:1.1s;-o-animation-duration:1.1s;-ms-animation-duration:1.1s;-webkit-animation-duration:1.1s;-moz-animation-duration:1.1s;animation-iteration-count:infinite;-o-animation-iteration-count:infinite;-ms-animation-iteration-count:infinite;-webkit-animation-iteration-count:infinite;-moz-animation-iteration-count:infinite;animation-direction:normal;-o-animation-direction:normal;-ms-animation-direction:normal;-webkit-animation-direction:normal;-moz-animation-direction:normal}#circularG_1{left:0;top:23px;animation-delay:.41s;-o-animation-delay:.41s;-ms-animation-delay:.41s;-webkit-animation-delay:.41s;-moz-animation-delay:.41s}#circularG_2{left:6px;top:6px;animation-delay:.55s;-o-animation-delay:.55s;-ms-animation-delay:.55s;-webkit-animation-delay:.55s;-moz-animation-delay:.55s}#circularG_3{top:0;left:23px;animation-delay:.69s;-o-animation-delay:.69s;-ms-animation-delay:.69s;-webkit-animation-delay:.69s;-moz-animation-delay:.69s}#circularG_4{right:6px;top:6px;animation-delay:.83s;-o-animation-delay:.83s;-ms-animation-delay:.83s;-webkit-animation-delay:.83s;-moz-animation-delay:.83s}#circularG_5{right:0;top:23px;animation-delay:.97s;-o-animation-delay:.97s;-ms-animation-delay:.97s;-webkit-animation-delay:.97s;-moz-animation-delay:.97s}#circularG_6{right:6px;bottom:6px;animation-delay:1.1s;-o-animation-delay:1.1s;-ms-animation-delay:1.1s;-webkit-animation-delay:1.1s;-moz-animation-delay:1.1s}#circularG_7{left:23px;bottom:0;animation-delay:1.24s;-o-animation-delay:1.24s;-ms-animation-delay:1.24s;-webkit-animation-delay:1.24s;-moz-animation-delay:1.24s}#circularG_8{left:6px;bottom:6px;animation-delay:1.38s;-o-animation-delay:1.38s;-ms-animation-delay:1.38s;-webkit-animation-delay:1.38s;-moz-animation-delay:1.38s}@keyframes bounce_circularG{0%{transform:scale(1)}100%{transform:scale(.3)}}@-o-keyframes bounce_circularG{0%{-o-transform:scale(1)}100%{-o-transform:scale(.3)}}@-ms-keyframes bounce_circularG{0%{-ms-transform:scale(1)}100%{-ms-transform:scale(.3)}}@-webkit-keyframes bounce_circularG{0%{-webkit-transform:scale(1)}100%{-webkit-transform:scale(.3)}}@-moz-keyframes bounce_circularG{0%{-moz-transform:scale(1)}100%{-moz-transform:scale(.3)}}"
+  }
+  function cssDivWithSpinner() {
+    return "<div class='spinner'><div id='circularG'><div id='circularG_1' class='circularG'></div><div id='circularG_2' class='circularG'></div><div id='circularG_3' class='circularG'></div><div id='circularG_4' class='circularG'></div><div id='circularG_5' class='circularG'></div><div id='circularG_6' class='circularG'></div><div id='circularG_7' class='circularG'></div><div id='circularG_8' class='circularG'></div></div></div></div>";
+  }
+  function appendStyle() {
+    $('<style type="text/css">' + cssStyleForSpinner() + '</style>').appendTo($('head'));
+  }
+
   function receiveMessage(event) {
     if (event.data.url) {
       setTimeout(function(){
@@ -46,18 +56,19 @@
   }
 
   BeGateway.prototype.showSpinner = function(){
-    $('#' + this.anchor).append(
-      "<div class='spinner'><img src='https://js.begateway.com/images/ajax-loader.gif' class='beSpinner'></img></div>");
+    $('#' + this.anchor).append(cssDivWithSpinner());
     $('.spinner').css('position', 'absolute').css('left', '45%').css('top', '28%');
   };
   BeGateway.prototype.hideSpinner = function(){
-    $('img.beSpinner').remove();
+    $('.spinner').remove();
   };
 
   BeGateway.prototype.prepareIframe = function (url) {
+    appendStyle();
+
     var iframeDiv = document.createElement("div");
     $(iframeDiv).attr("id", this.iframeDivId);
-    
+
     this.showSpinner();
     $(iframeDiv).hide();
 
